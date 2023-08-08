15ebe10cc382034c5f6be4c539eb2a8f72081cc4
sldev2 <sldev2@users.noreply.github.com>
2023-08-08 16:05:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember()  {
+  var skills = ['HTML', 'CSS', 'JS'];
+  var skillsLength = skills.length;
+  for (var i = 0; i < skillsLength; i++) {
+    console.log(skills[i]);
+  }
+}
